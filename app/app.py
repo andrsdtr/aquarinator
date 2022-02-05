d7ef346e3a5880dc6fa4df4ceb65154a3b343333
adichter-hpe <andreas.dichter@hpe.com>
2022-02-06 00:52:42 +0100

    added average calc + db integration + several bug fixes + started commenting + last irrigation

--- app/app.py
+++ app/app.py
@@ -23,62 +23,87 @@ firebase = pyrebase.initialize_app(config)
 
 db = firebase.database()
 
-def add_measurement(moisture, watered): 
-    timestamp = str(datetime.now())
-    time = str(timestamp)[11:-10]
+def add_measurement(moisture, watered):
+    '''
+    Function to add a new measurement to db (moisture, timestamp, key = timestamp without symbols)
+    It also checks if the plant got watered after measurement
+        => if that is the case it enters the necessary entrys in db 
+           (How much water has been used?, updating total used water for average use calculation, timestamp, updating how many liters are left in bucket)
+    '''
+    timestamp = str(datetime.now())[:-7]
+    time = str(timestamp)[11:-3]
     data = db.child('moisture_mesurements').get()
     liters_left = db.child('liters_left').get().val()
     pump_use = db.child('pump_use').get().val()
     liter_state = liters_left['liters_left']
-    
-    #if liters_left is None or liters_left['liters_left'] == 'Not available':
-    #    if water_capacity is None:
-    #        db.child('liters_left').set({'liters_left': 'Not available'})
-    #    else:
-    #        db.child('liters_left').set({'liters_left': water_capacity['water_capacity']})
-
-    #if watered == True:
-    #    if water_capacity is None or pump_use is None or liters_left is None or liters_left ['liters_left'] == 'Not available':
-    #        db.child('liters_left').set({'liters_left': 'Not available'})
-    #    else:
-    #        new_state = int(liters_left['liters_left']) - (int(pump_use['pump_use'])/1000)
-    #        db.child('liters_left').set({'liters_left': new_state})
+    key = re.sub('\.|\-|\:|\ ', '', str(datetime.now()))
 
     if watered == True:
         liter_state = round((liters_left['liters_left'] - pump_use['pump_use']/1000), 1)
         db.child('liters_left').set({'liters_left': liter_state})
-        db.child('watered').set({'watered': liter_state})
-
-    key = re.sub('\.|\-|\:|\ ', '', str(datetime.now()))
+        watered_with = pump_use['pump_use']
+        db.child('watered').child('watered').child(key).set({'watered_at': timestamp, 'watered_with': watered_with})
+        if db.child('watered').child('total').get().val() is None:
+            total = 0
+        else:
+            total = db.child('watered').child('total').get().val()['total']
+        total += watered_with/1000
+        db.child('watered').child('total').set({'total': total})
     
     data = {'time': time, 'timestamp': timestamp, 'moisture': moisture, 'liters_left': liter_state}
     db.child('moisture_mesurements').child(key).set(data)
 
-def get_labels_values(data):
+def get_labels_values():
+    data = db.child('moisture_mesurements').get().each()
     labels = []
     values = []
-    pro_data = []
-    if data.val() is None:
-        return [],[]
-    for i in data.each():
-        pro_data.append(i.val())
-    if len(pro_data) <= 30:
-        i = len(pro_data)
-        j = 0
-        while i > 0:
-            labels.append(pro_data[j]['time'])
-            values.append(pro_data[j]['moisture'])
-            i-=1
-            j+=1
-    elif len(pro_data) > 30:
-        i = 30
-        j = len(pro_data) - 30
-        while i > 0:
-            labels.append(pro_data[j]['time'])
-            values.append(pro_data[j]['moisture'])
-            i-=1
-            j+=1
+    if data is not None:
+        if len(data) <= 30:
+            i = len(data)
+            j = 0
+            while i > 0:
+                labels.append(data[j].val()['time'])
+                values.append(data[j].val()['moisture'])
+                i-=1
+                j+=1
+        elif len(data) > 30:
+            i = 30
+            j = len(data) - 30
+            while i > 0:
+                labels.append(data[j].val()['time'])
+                values.append(data[j].val()['moisture'])
+                i-=1
+                j+=1
     return labels, values
+
+def get_average():
+    watered = db.child('watered').child('watered').get().each()
+    if watered is not None:
+        if len(watered) > 1:
+            total = db.child('watered').child('total').get().val()['total']
+            first_str = watered[0].val()['watered_at']
+            last_str = watered[-1].val()['watered_at']
+            first = datetime.strptime(first_str , "%Y-%m-%d %H:%M:%S")
+            last = datetime.strptime(last_str , "%Y-%m-%d %H:%M:%S")
+            seconds = (last - first).total_seconds()
+            weeks = seconds/60/60/24/7
+            average = round(total/weeks, 3)
+            return str(average)+'l'
+        else:
+            return 'not available yet'
+    else:
+            return 'not available yet'
+
+def get_last_irrigation():
+    watered = db.child('watered').child('watered').get().each()
+    if watered is not None:
+        if len(watered) > 0:
+            last_str = watered[-1].val()['watered_at']
+            return last_str
+        else:
+            return '-'
+    else:
+        return '-'
 
 @app.route('/', methods = ['GET', 'POST'])
 def base_control():
@@ -86,12 +111,18 @@ def base_control():
     water_capacity = db.child('water_capacity').get().val()
 
     if request.form.get('pump_use') == "pump_use":
-        pump_use = int(request.form.get('pump_value'))
-        db.child('pump_use').set({'pump_use': pump_use})
+        if (request.form.get('pump_value')) == '':
+            pass
+        else:
+            pump_use = int(request.form.get('pump_value'))
+            db.child('pump_use').set({'pump_use': pump_use})
     elif request.form.get('water_capacity') == "water_capacity":
-        water_capacity = int(request.form.get('water_value'))
-        db.child('water_capacity').set({'water_capacity': water_capacity})
-        db.child('liters_left').set({'liters_left': water_capacity})
+        if request.form.get('water_value') == '':
+            pass
+        else:
+            water_capacity = int(request.form.get('water_value'))
+            db.child('water_capacity').set({'water_capacity': water_capacity})
+            db.child('liters_left').set({'liters_left': water_capacity})
     elif request.form.get('water') == 'water':
         i = 1
         # adding random values to db:
@@ -103,6 +134,8 @@ def base_control():
         # Hier bewässerungsfunktion einfügen
         #water.pump_on()
 
+    last_irrigation = get_last_irrigation()
+
     if pump_use is None or water_capacity is None:
         if db.child('pump_use').get().val() is None:
             pump_use = 'Enter new Value in '
@@ -112,12 +145,14 @@ def base_control():
             water_capacity = 'Enter new Value in '
         elif db.child('water_capacity').get().val() is not None:
             water_capacity = db.child('water_capacity').get().val()['water_capacity']
+        if db.child('pump_use').get().val() is not None and db.child('water_capacity').get().val() is not None:
+            # Hier Loop start einfügen
+            pass
         return render_template('start_config.html',
                                 pump_use = pump_use,
                                 water_capacity = water_capacity)
 
-    data = db.child('moisture_mesurements').get()
-    labels, values = get_labels_values(data)
+    labels, values = get_labels_values()
     if len(values) > 0 :
         humidity = str(int(values[-1]*100))+ '%'
     elif len(values) == 0:
@@ -130,13 +165,14 @@ def base_control():
             liter_state = 'Bucket empty'
         else: 
             liter_state = str(liters_left['liters_left'])+'l'
-    average_time = 3
+    average_use = get_average()
     return render_template('base_control.html',
                             labels = labels,
                             values = values,
                             humidity = humidity,
                             liter_state = liter_state,
-                            average_time = average_time)
+                            average_use = average_use,
+                            last_irrigation = last_irrigation)
 
 @app.route('/advanced', methods=['POST', 'GET'])
 def advanced():
@@ -147,12 +183,18 @@ def advanced():
             pro_data.append(i.val())
     
     if request.form.get('pump_use') == "pump_use":
-        pump_use = int(request.form.get('pump_value'))
-        db.child('pump_use').set({'pump_use': pump_use})
+        if (request.form.get('pump_value')) == '':
+            pass
+        else:
+            pump_use = int(request.form.get('pump_value'))
+            db.child('pump_use').set({'pump_use': pump_use})
     elif request.form.get('water_capacity') == "water_capacity":
-        water_capacity = int(request.form.get('water_value'))
-        db.child('water_capacity').set({'water_capacity': water_capacity})
-        db.child('liters_left').set({'liters_left': water_capacity})
+        if request.form.get('water_value') == '':
+            pass
+        else:
+            water_capacity = int(request.form.get('water_value'))
+            db.child('water_capacity').set({'water_capacity': water_capacity})
+            db.child('liters_left').set({'liters_left': water_capacity})
 
     if db.child('pump_use').get().val() is None:
         pump_use = 'Enter new Value in '
